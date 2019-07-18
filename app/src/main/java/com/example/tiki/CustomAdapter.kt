302f3c7774d958a5259f7fae8c0607307da6a322
viet.nguyen <vietnguyen@siliconprime.com>
2019-07-18 13:11:26 +0700

    Fix task 2

--- app/src/main/java/com/example/tiki/CustomAdapter.kt
+++ app/src/main/java/com/example/tiki/CustomAdapter.kt
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.item.view.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -23,9 +25,58 @@ class CustomAdapter(var items: ArrayList<String>, var context: Context) :
         return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
     }
 
+    private fun updateText(text: String): String? {
+        val textList: List<String> = text.split(" ")
+        if (textList.size == 1) {
+            return text
+        } else if (textList.size == 2) {
+            return "".plus(textList[0]).plus("\n").plus(textList[1])
+        } else {
+
+            var num = 0
+
+            for (i in 0 until textList.size) {
+                num += textList[i].length
+            }
+
+            var firstPart = 0
+            var secondPart: Int
+            var bestfreference: Int = num
+
+            var updateText = ""
+
+            for (i in 0 until textList.size) {
+                firstPart += textList[i].length
+                secondPart = num - firstPart
+                if (secondPart - firstPart > 0) {
+                    bestfreference = secondPart - firstPart
+                } else {
+                    val index = if (firstPart - secondPart < bestfreference) {
+                        i
+                    } else {
+                        i - 1
+                    }
+                    updateText = ""
+                    for (j in 0..index) {
+                        updateText += textList[j] + " "
+                    }
+                    updateText = updateText.dropLast(1)
+                    updateText += "\n"
+
+                    for (j in index + 1 until textList.size) {
+                        updateText += textList[j] + " "
+                    }
+                    updateText = updateText.dropLast(1)
+                    break
+                }
+            }
+            return updateText
+        }
+    }
+
     override fun onBindViewHolder(keywordViewHolder: KeywordViewHolder, position: Int) {
 
-        keywordViewHolder.nameKeyword.text = items[position]
+        keywordViewHolder.nameKeyword.text = updateText(items[position])
         keywordViewHolder.background.setBackgroundColor(getRandomColor())
     }
 
@@ -39,6 +90,6 @@ class CustomAdapter(var items: ArrayList<String>, var context: Context) :
 }
 
 class KeywordViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    var background = view.bg
-    var nameKeyword = view.tvName
+    var background: LinearLayout = view.bg
+    var nameKeyword: TextView = view.tvName
 }
